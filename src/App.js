5bd9df2952205339a521b51261b934ed3fa32e70
Aris Empanta <eams220891@gmail.com>
2022-08-06 10:02:47 +0300

    trials

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
 import axios from "axios"
+import { useState } from  'react'
 
 function App() {
+
+  const [food, setFood] = useState("")
 
   const getFood = () => {
     
     axios.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita").then((res) => {
-      console.log(res.data[0])
+      setFood(res.data[0])
     }).catch((err) => console.log(err))
   }
 
   return (
     <div>
-      <button onClick={ getFood }>GET FOOD</button>
+      <button onClick={ getFood }>{ food }</button>
     </div>
   );
 }
