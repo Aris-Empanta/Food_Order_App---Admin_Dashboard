3e65b228655079a6e5a625634bde3103d5f8608e
Aris Empanta <eams220891@gmail.com>
2022-08-07 18:09:02 +0300

    axios post config

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from  'react'
 
 function App() {
 
+  const [category, setCategory] = useState("pizza")
   const [food, setFood] = useState("")
 
   const getFood = () => {
@@ -13,10 +14,25 @@ function App() {
     }).catch((err) => console.log(err))
   }
 
+
+  const sendCategory = () => {
+     let category = document.getElementById("foodCategory").value
+
+     if(category) {
+        console.log(category)
+     }     
+     axios.post("https://restaurant-builder-server.herokuapp.com/foods", {category : category}).then((res) => {
+      console.log(res)
+    })
+  }
+  
+
   return (
     <div>
       <button onClick={ getFood }>add food</button>
       <p>{ food }</p>
+      <input type="text" placeholder='Add food category' id="foodCategory"/><br></br>
+      <button onClick= { sendCategory }> SEND CATEGORY </button>
     </div>
   );
 }
