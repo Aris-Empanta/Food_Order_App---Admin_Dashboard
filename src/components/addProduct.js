4074adb44ffe5a9c7884c80d99bf60f304d0c593
Aris Empanta <eams220891@gmail.com>
2022-08-09 20:01:13 +0300

    daily updates

--- src/components/addProduct.js
+++ src/components/addProduct.js
@@ -1,9 +1,34 @@
 import "../css/addProduct.css"
+import axios from "axios"
+import { useState } from  'react'
 
 export const AddProduct = () => {
 
+       const addProduct = () => {
+
+            let category = document.getElementById("categoryInput").value
+            let name = document.getElementById("nameInput").value
+            let currency = document.getElementById("currencySelect").value
+            let quantity = document.getElementById("quantityInput").value
+            let deliveryPrice = document.getElementById("deliveryInput").value
+            let takeAwayPrice = document.getElementById("takeAwayInput").value
+            let description = document.getElementById("descriptionInput").value
+            let image = document.getElementById("imageInput").value
+
+            axios.post("http://localhost:5000/products", {
+                    category: category,
+                    name: name,
+                    currency: currency,
+                    quantity: quantity,
+                    deliveryPrice: deliveryPrice,
+                    takeAwayPrice: takeAwayPrice,
+                    description: description,
+                    image: image
+            })
+        }
+
     return(<div className="addProduct">
-                <form>
+                <form enctype="multipart/form-data" action="/#/add-product" method="post">
                     <h1 id="formTitle" className="formComponents" >Add Products</h1>
                     <hr id="formLine"></hr>                    
                     <label id="categoryLabel" className="formComponents" >Product Category</label>
@@ -19,14 +44,13 @@ export const AddProduct = () => {
                     <input placeholder="01" id="quantityInput" className="formComponents"  />
                     <label id="deliveryLabel"  className="formComponents" >Delivery Price</label>
                     <input placeholder="$10" id="deliveryInput"  className="formComponents" />
-                    <label id="priceLabel" className="formComponents" >Take away Price</label>
-                    <input placeholder="$10" id="priceInput" className="formComponents" />
+                    <label id="takeAwayLabel" className="formComponents" >Take away Price</label>
+                    <input placeholder="$10" id="takeAwayInput" className="formComponents" />
                     <label id="descriptionLabel" className="formComponents" >Description</label>
                     <textarea id="descriptionInput" placeholder="Description" className="formComponents" />
                     <label id="imageLabel" className="formComponents" >Product Image</label>
-                    <input type="file" id="imageInput"  className="formComponents" 
-                            accept="image/png, image/jpeg, image/jpg"></input>
-                    <input type="submit" id="submit" className="formComponents" />
+                    <input type="file" name="image" id="imageInput"  className="formComponents" ></input>
+                    <button type="submit" id="submit" className="formComponents" onClick={ addProduct }>add product</button>
                 </form>
            </div>)
 }
