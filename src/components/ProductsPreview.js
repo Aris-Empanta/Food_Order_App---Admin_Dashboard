925e5259d1e4f2e76ef352c85e4ce47551736ccc
Aris Empanta <eams220891@gmail.com>
2022-08-11 19:48:46 +0300

    daily configs

--- src/components/ProductsPreview.js
+++ src/components/ProductsPreview.js
@@ -1,9 +1,36 @@
 import "../css/preview.css"
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 //The component where we can check and modify the menu.
 export const Preview = () => {
 
+    const [products, setProducts ] = useState([])
+    
+
+    useEffect(() => {
+
+        axios.get("http://localhost:5000/products").
+        then((res) => {           
+                
+                setProducts(res.data)                   
+
+               }
+             )
+           } , [setProducts]
+        )
+
     return(<div className="preview">
-                <h1>preview</h1>
+            <div id="productsWrapper">
+            {products.map((item) => <div className="products">
+                                        <img src="http://localhost:5000/images/1660216422033-pizza_margarita.jpg"/>
+                                        <p>{item.Category}</p>
+                                        <p>{item.Name}</p>
+                                        <p>{item.Delivery_price}</p>
+                                        <p>{item.Take_away_price}</p>
+                                        <p>{item.Currency}</p>
+                                        <p>{item.Description}</p>
+                                    </div>)}
+            </div>
            </div>)
 }
