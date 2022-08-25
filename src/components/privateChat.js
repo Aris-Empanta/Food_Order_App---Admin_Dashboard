df44613a92a13cc478e28051141295d1d055ecfb
Aris Empanta <eams220891@gmail.com>
2022-08-25 04:40:14 +0300

    Chat functions properly - saves and retrieves  messages

--- src/components/privateChat.js
+++ src/components/privateChat.js
@@ -1,24 +1,37 @@
 import "../css/chat.css"
 import io from 'socket.io-client';
 import {useParams} from 'react-router-dom';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { showMessage } from "../functions/chat";
+import axios from "axios"
 
 export const PrivateChat = () => {
+
+    const [messagesHistory, setMessagesHistory ] = useState([])
 
     const socket = io(`http://localhost:5000`)
     const params = useParams();
 
     useEffect(() => {
-        socket.on('customer '+ params.customer, (data)=> {  let sender = data.sender === 'admin' ? 'me' : data.sender
-                                                            let message = data.message
-                                                            showMessage(sender, message)
-                                                        })
-                    })
+                    let customer = params.customer 
+
+                    axios.get('http://localhost:5000/chat-messages')
+                         .then( res => {
+                                        let messages = res.data.filter(item => item.Customer === customer)
+                                        if(messagesHistory.length !== messages.length) setMessagesHistory(messages)                                       
+                                        })
+                    
+                    socket.on('customer '+ params.customer, (data)=> {  let sender = data.sender === 'admin' ? 'me' : data.sender
+                                                                        let message = data.message
+                                                                        if(data.sender !== 'admin') showMessage(sender, message)
+                                                                        console.log(0)
+                                                                    })
+                    
+                    }, [])
 
     const sendMessage = (e) => {
         e.preventDefault()
-
+        
         let username = params.customer
         let message = document.getElementById("input").value
         let data = { username: username,
@@ -27,10 +40,13 @@ export const PrivateChat = () => {
                     }
         
         socket.emit('chat message', data)
+        axios.post('http://localhost:5000/chat-messages', data )
+        showMessage("me", message)
     }
 
      return(<div className="chat">
-                <ul id="messages">                    
+                <ul id="messages">
+                    { messagesHistory.map( item => <li>{(item.Sender === "admin" ? "me" : item.Sender) + ": " + item.Message}</li>) }                    
                 </ul>
                 <form id="form" action="" onSubmit={sendMessage}>
                     <input id="input" /><button>Send</button>
--- src/functions/chat.js
+++ src/functions/chat.js
@@ -1,8 +1,9 @@
 export const showMessage = (name, msg) => {
-            
+    
     let message = document.createElement("li")
     message.innerHTML = name + ": " + msg
     
     let messages = document.getElementById("messages")
     messages.appendChild(message)
+    console.log(1)
  }
