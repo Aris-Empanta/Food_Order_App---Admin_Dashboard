a31751140ffd3c9fe466defe90c691411be3507a
Aris Empanta <eams220891@gmail.com>
2022-09-25 14:25:55 +0300

    Completed product category section

--- src/components/allCategories.js
+++ src/components/allCategories.js
@@ -1,10 +1,13 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import "../css/allCategories.css"
+import { useNavigate } from "react-router-dom";
 
 export const AllCategories = () => {
 
     const [ categories, setCategories ] = useState([])
+
+    const navigate = useNavigate()
 
     useEffect(() => {
 
@@ -12,12 +15,22 @@ export const AllCategories = () => {
              .then( res => setCategories(res.data))
     }, [])
 
+    const checkDishes = (category) => {
 
-    return (<div className="allCategories">
-                { categories.map( item => <div>
-                                            <p>{ item.category }</p>
+        navigate("./" + category, { replace: false} )
+    }
+
+
+    return (<div>
+              <div id="typesTitle">
+                <h1>Dish types</h1>
+              </div>
+              <div className="allCategories">
+                { categories.map( item => <div className="categoryWrapper">                                            
                                             < img className="categoryImage" src={ item.image } />
-                                            <a href={ "#/all-categories/" + item.category } target="_blank"><button>Check all</button></a>
-                                        </div>) }
+                                            <p className="categoryTitle">{ item.category }</p>
+                                            <button onClick={() => checkDishes(item.category)} className="checkDishes">Check dishes</button>                                            
+                                          </div>) }
+              </div>
             </div>)
 }
